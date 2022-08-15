bbaee133b842c8d94fb52e56c64353a4117612a9
agychko <agychko@gmail.com>
2022-08-16 00:26:02 +0300

    users component created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { V1Module } from './modules/v1/v1.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [V1Module],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+// import { Module } from '@nestjs/common';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { UsersModule } from './modules/v1/users/users.module';
+
+// @Module({
+//   imports: [
+//     MongooseModule.forRoot('mongodb://localhost:27017/users_db'),
+//     UsersModule,
+//   ],
+// })
+// export class AppModule {}
