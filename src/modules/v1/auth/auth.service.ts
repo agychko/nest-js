75c2f613e01ee7571f8a11821f443928d3447597
agychko <agychko@gmail.com>
2022-08-25 16:06:41 +0300

    auth module start

---
+++ src/modules/v1/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../users/schemas/users.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, email: string): Promise<User> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && user.fullName === username) {
+      const { ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: User) {
+    const payload = { username: user.fullName, email: user.email };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
