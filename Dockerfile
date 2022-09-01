676f1fadba3ff2802660636f0b5dced053c42299
agychko <agychko@gmail.com>
2022-09-01 14:19:29 +0300

    feat(add Docker file): add docker-compose and fix bags

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM node:16
+FROM node:16-alpine
 
 WORKDIR /usr/src/app
 
 COPY package*.json ./
 
-RUN ci --only=production
+RUN npm ci --only=production
 
 COPY . .
 
--- src/modules/v1/v1.module.ts
+++ src/modules/v1/v1.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/users_db'),
+    MongooseModule.forRoot('mongodb://mongodb:27017/app'),
     UsersModule,
     AuthModule,
   ],
