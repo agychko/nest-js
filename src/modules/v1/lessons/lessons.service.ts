da903a346b287654a802aa17061ce0868a598d58
agychko <agychko@gmail.com>
2023-07-18 12:15:22 +0300

    modified response message in app controllers

--- src/modules/v1/lessons/lessons.service.ts
+++ src/modules/v1/lessons/lessons.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Lesson } from "./schemas/lessons.schema";
 import { CreateLessonDto } from "./dto/create-lesson.dto";
 import { UpdateLessonDto } from "./dto/update-lesson.dto";
@@ -6,17 +6,25 @@ import { LessonsRepository } from "./lessons.repository";
 
 @Injectable()
 export class LessonService {
-    constructor(private readonly lessonsRepository: LessonsRepository) {}
+    constructor(private readonly lessonsRepository: LessonsRepository) { }
 
     async create(createLessonDto: CreateLessonDto): Promise<Lesson> {
         return this.lessonsRepository.create(createLessonDto);
     }
 
     async findAll(): Promise<Lesson[]> {
-        return this.lessonsRepository.findAll();
+        const lessonsData = await this.lessonsRepository.findAll();
+        if (!lessonsData || lessonsData.length === 0) {
+            throw new NotFoundException(`Lessons not found!`)
+        }
+        return lessonsData;
     }
 
-    async updateById(_id: string, updateLessonDto: UpdateLessonDto ): Promise<Lesson> {
-        return this.lessonsRepository.updateById(_id, updateLessonDto);
+    async updateById(_id: string, updateLessonDto: UpdateLessonDto): Promise<Lesson> {
+        const exsistingLesson = await this.lessonsRepository.updateById(_id, updateLessonDto);
+        if (!exsistingLesson) {
+            throw new NotFoundException(`Lesson with id: ${_id} not exsist!`)
+        }
+        return exsistingLesson;
     }
 }
